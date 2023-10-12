27c5da9b181d70b26f331be2e5555c691915d7cd
​Andrzej Ressel <github@andrzejressel.pl>
2023-10-13 01:58:31 +0200

    Add new function type - function with context
    Spotless + integration tests

--- lib/src/main/java/pl/andrzejressel/deeplambdaserialization/lib/SerializableInputOutputFunctionWithContextN.java
+++ lib/src/main/java/pl/andrzejressel/deeplambdaserialization/lib/SerializableInputOutputFunctionWithContextN.java
@@ -2,14 +2,15 @@ package pl.andrzejressel.deeplambdaserialization.lib;
 
 import pl.andrzejressel.dto.serializator.Serializator;
 
-public abstract class SerializableInputOutputFunctionWithContextN<CONTEXT> extends SerializableInputFunctionWithContextN<CONTEXT> {
+public abstract class SerializableInputOutputFunctionWithContextN<CONTEXT>
+    extends SerializableInputFunctionWithContextN<CONTEXT> {
   public abstract Serializator<Object> getOutputSerializator();
 
-  public byte[] executeAndSerialize(CONTEXT context,Object[] args) {
+  public byte[] executeAndSerialize(CONTEXT context, Object[] args) {
     return getOutputSerializator().serialize(execute(context, args)).array();
   }
 
-  public byte[] executeAndSerialize(CONTEXT context,byte[][] serializedArgs) {
+  public byte[] executeAndSerialize(CONTEXT context, byte[][] serializedArgs) {
     return getOutputSerializator().serialize(execute(context, serializedArgs)).array();
   }
 }
