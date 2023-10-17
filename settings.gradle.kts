99fc89a008045d6ce171727b6976cb1e7411f116
​Andrzej Ressel <github@andrzejressel.pl>
2023-10-17 20:07:09 +0200

    Agree to Gradle Enterprise stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,14 @@
+plugins { id("com.gradle.enterprise") version ("3.15.1") }
+
+if (!System.getenv("CI").isNullOrEmpty()) {
+  gradleEnterprise {
+    buildScan {
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
+    }
+  }
+}
+
 rootProject.name = "deep-lambda-serialization"
 
 include(":gradle-plugin", ":lib", ":serializator", ":entrypoint", ":lib-kotlin")
